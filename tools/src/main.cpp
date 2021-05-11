ef1362530389c4f1c4075b787b805b1ef352cf1b
JinlongHe <loringhe@outlook.com>
2021-05-11 11:23:53 +0800

    fix: modify test case in simle

--- tools/src/main.cpp
+++ tools/src/main.cpp
@@ -10,19 +10,20 @@ int main (int argc, char* argv[]) {
     ASMParser::parseManifest(argv[1], &a);
     ASMParser::parseATG(argv[2], &a);
     ASMParser::parseFragment(argv[3], &a);
-    Configuration<Fragment> config;
     a.fomalize();
+    bool flag = true;
+    std::ofstream out(argv[4]);
     for (auto& [name, activity] : a.getActivityMap()) {
         if (a.getActivityTransactionMap().count(activity) != 0) {
             if (a.getFragmentTransactionMap(activity).size() > 0) {
+                flag = false;
                 FragmentAnalyzer analyzer(2,3,&a,activity);
-                std::ofstream out(argv[4]);
                 analyzer.analyzeBoundedness(out);
-                //ASMParser::parseFragmentConfig(argv[4], &a, &config);
-                //auto& content = config.getContent();
-                //analyzer.analyzeReachability(activity, content[0].first, content[0].second);
             }
         }
+    }
+    if (flag) {
+        out << "No Fragments" << endl;
     }
     //ASMParser::parseATG(argv[2], &a);
     //MultiTaskAnalyzer::analyzeReachability(&a, 5);
